d7008e52332257122790cc04e48bb04cc4073090
Nguyễn Quang An <ngquangan@gmail.com>
2021-03-08 12:54:44 +0700

    WIP: Create atom family

--- src/components/Rectangle/Rectangle.tsx
+++ src/components/Rectangle/Rectangle.tsx
@@ -1,5 +1,5 @@
-import {useContext} from 'react'
-import {ElementsContext, SelectedElementContext} from '../../Canvas'
+import { atomFamily, useRecoilState } from 'recoil'
+import {selectedElementState} from '../../Canvas'
 import {Drag} from '../Drag'
 import {RectangleContainer} from './RectangleContainer'
 import {RectangleInner} from './RectangleInner'
@@ -11,15 +11,24 @@ export type ElementStyle = {
 
 export type Element = {style: ElementStyle}
 
-export const Rectangle = ({element, index}: {element: Element; index: number}) => {
-    const {selectedElement, setSelectedElement} = useContext(SelectedElementContext)
-    const {setElement} = useContext(ElementsContext)
+const elementState = atomFamily<Element, number>({
+    key: 'element',
+    default: {
+        style: {
+            position: { top: 0, left: 0 },
+            size: { width: 50, height: 50 }
+        }
+    }
+});
 
+export const Rectangle = ({id}: {id: number}) => {
+    const [selectedElement, setSelectedElement] = useRecoilState(selectedElementState);
+    const [element, setElement] = useRecoilState(elementState(id));
     return (
         <Drag
             position={element.style.position}
             onDrag={(position) => {
-                setElement(index, {
+                setElement({
                     style: {
                         ...element.style,
                         position,
@@ -32,10 +41,10 @@ export const Rectangle = ({element, index}: {element: Element; index: number}) =
                     position={element.style.position}
                     size={element.style.size}
                     onSelect={() => {
-                        setSelectedElement(index)
+                        setSelectedElement(id)
                     }}
                 >
-                    <RectangleInner selected={index === selectedElement} />
+                    <RectangleInner selected={id === selectedElement} />
                 </RectangleContainer>
             </div>
         </Drag>
